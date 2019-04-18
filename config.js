7326f685b8c20796abd7c232da091774e43874bc
Miguel Llufriu <miguelllufriu@gmail.com>
2019-04-18 18:59:22 +0200

    Added style.css file and a super cool background!

--- config.js
+++ config.js
@@ -3,12 +3,14 @@ module.exports = {
         short: 100,
         medium: 500,
         long: 1000
-    }, firebase_config: {
+    },
+    firebase_config: {
       apiKey: "AIzaSyBhHQtskONKLeKIdZuaCygwSr09ImgQBac",
       authDomain: "systemmonitor-2b015.firebaseapp.com",
       databaseURL: "https://systemmonitor-2b015.firebaseio.com",
       projectId: "systemmonitor-2b015",
       storageBucket: "systemmonitor-2b015.appspot.com",
       messagingSenderId: "153297484955"
-  }, firebase_path: "system-info" 
+    }, 
+    firebase_path: "system-info" 
 }
